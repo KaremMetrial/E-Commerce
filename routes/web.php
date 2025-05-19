86f234470d52f2401ce5e156886c6b70b398350d
KaremMetrial <karim.metrial@gmail.com>
2025-05-19 22:37:26 +0300

    sidebar - switch language btn

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+    use Illuminate\Support\Facades\Route;
+    use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+    Route::group(['prefix' => LaravelLocalization::setLocale()], function () {
+        Route::get('/', function () {
+            return view('dashboard.welcome');
+        });
+    });
